9686518cd4ffd2e76ad5e552e3e59bf6f3497f44
tahsin <amin1996bangladesh>
2020-11-24 18:05:12 +0600

    Commenting out server processor to match the start app more closely

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 
 'use strict';
 
-var fs = require('fs');
+
 var bGround = require('fcc-express-bground');
 var myApp = require('./myApp');
 var express = require('express');
@@ -24,36 +24,36 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
 
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.route('/_api/package.json')
-  .get(function(req, res, next) {
-    console.log('requested');
-    fs.readFile(__dirname + '/package.json', function(err, data) {
-      if(err) return next(err);
-      res.type('txt').send(data.toString());
-    });
-  });
-
-app.route('/')
-    .get(function(req, res) {
-		  res.sendFile(process.cwd() + '/views/index.html');
-    })
-
-// Respond not found to all the wrong routes
-app.use(function(req, res, next){
-  res.status(404);
-  res.type('txt').send('Not found');
-});
-
-// Error Middleware
-app.use(function(err, req, res, next) {
-  if(err) {
-    res.status(err.status || 500)
-      .type('txt')
-      .send(err.message || 'SERVER ERROR');
-  }
-})
+// app.use('/public', express.static(process.cwd() + '/public'));
+//
+// app.route('/_api/package.json')
+//   .get(function(req, res, next) {
+//     console.log('requested');
+//     fs.readFile(__dirname + '/package.json', function(err, data) {
+//       if(err) return next(err);
+//       res.type('txt').send(data.toString());
+//     });
+//   });
+//
+// app.route('/')
+//     .get(function(req, res) {
+// 		  res.sendFile(process.cwd() + '/views/index.html');
+//     })
+//
+// // Respond not found to all the wrong routes
+// app.use(function(req, res, next){
+//   res.status(404);
+//   res.type('txt').send('Not found');
+// });
+//
+// // Error Middleware
+// app.use(function(err, req, res, next) {
+//   if(err) {
+//     res.status(err.status || 500)
+//       .type('txt')
+//       .send(err.message || 'SERVER ERROR');
+//   }
+// })
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function () {
   console.log('Node.js listening on port '+ port + '...');
