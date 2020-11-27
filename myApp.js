550de6c4b03a530b97c0dfa6700019ee1699ff2a
tahsin <amin1996bangladesh>
2020-11-27 10:58:31 +0600

    Organise application function to myApp.js

--- myApp.js
+++ myApp.js
@@ -8,13 +8,23 @@ var bGround = require('fcc-express-bground');
 
 
 
+// 1) Meet the node console.
 console.log("Hello World");
 bGround.log("Hello World");
 
+// 2) A first working Express Server
 
 
+// 3) Serve an html file.
 
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/views/index.html");
+});
 
+// 4) Serve the static assets
+
+app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
